6fd32210b1cd5e90408b46cf32ca7cc3b471bbc7
Ruslan <rsekirin@prendehealth.com>
2023-09-29 00:43:51 +0300

    completed task

---
+++ Smartwyre.DeveloperTest.Runner/Program.cs
@@ -0,0 +1,35 @@
+﻿using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Interfaces;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Types;
+using System;
+
+namespace Smartwyre.DeveloperTest.Runner;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter rebate details:");
+        string rebateId = Console.ReadLine();
+        string productId = Console.ReadLine();
+
+        // Створення інстанції RebateDataStore і ProductDataStore (можливо, вам потрібно використовувати реальні реалізації цих інтерфейсів)
+        var rebateDataStore = new RebateDataStore();
+        var productDataStore = new ProductDataStore();
+        var iniciativeCalculator = new IncentiveCalculators();
+        CalculateRebateRequest request = new CalculateRebateRequest()
+        {
+            RebateIdentifier = rebateId,
+            ProductIdentifier = productId,
+            Volume = 500
+        };
+
+        // Передача інстанцій rebateDataStore і productDataStore в конструктор RebateService
+        RebateService rebateService = new RebateService(rebateDataStore, productDataStore, iniciativeCalculator);
+
+        var rebateAmount = rebateService.Calculate(request);
+
+        Console.WriteLine($"Rebate Amount: {rebateAmount.RebateAmount}");
+    }
+}
---
+++ Smartwyre.DeveloperTest.Tests/PaymentService.Tests.cs
@@ -0,0 +1,204 @@
+using Moq;
+using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Interfaces;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Types;
+using System;
+using Xunit;
+
+namespace Smartwyre.DeveloperTest.Tests;
+
+public class PaymentServiceTests
+{
+
+    public class RebateServiceTests
+    {
+        [Fact]
+        public void Calculate_WhenRebateIsNull_ReturnsResultWithFalseSuccess()
+        {
+            // Arrange
+            var rebateDataStoreMock = new Mock<IRebateDataStore>();
+            var productDataStoreMock = new Mock<IProductDataStore>();
+            var incentiveCalculatorsMock = new Mock<IIncentiveCalculators>();
+
+            var rebateService = new RebateService(rebateDataStoreMock.Object, productDataStoreMock.Object, incentiveCalculatorsMock.Object);
+
+            rebateDataStoreMock.Setup(rds => rds.GetRebate(It.IsAny<string>())).Returns((Rebate)null);
+
+            // Act
+            var request = new CalculateRebateRequest();
+            var result = rebateService.Calculate(request);
+
+            // Assert
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public void Calculate_FixedCashAmount_ReturnsSuccess()
+        {
+            // Arrange
+            var rebateDataStoreMock = new Mock<IRebateDataStore>();
+            var productDataStoreMock = new Mock<IProductDataStore>();
+            var incentiveCalculatorsMock = new IncentiveCalculators();
+
+            rebateDataStoreMock.Setup(r => r.GetRebate(It.IsAny<string>()))
+                .Returns((string rebateIdentifier) =>
+                {
+                    return new Rebate()
+                    {
+                        Incentive = IncentiveType.FixedCashAmount,
+                        Amount = 10m
+                    };
+                });
+
+            productDataStoreMock.Setup(p => p.GetProduct(It.IsAny<string>()))
+                .Returns((string productIdentifier) =>
+                {
+                    return new Product
+                    {
+                        SupportedIncentives = SupportedIncentiveType.FixedCashAmount
+                    };
+                });
+
+            var request = new CalculateRebateRequest
+            {
+                RebateIdentifier = "1",
+                ProductIdentifier = "A",
+                Volume = 500
+            };
+            var rebate = new Rebate
+            {
+                Incentive = IncentiveType.FixedCashAmount,
+                Amount = 10m
+            };
+
+            var product = new Product
+            {
+                SupportedIncentives = SupportedIncentiveType.FixedCashAmount
+            };
+            var result = new CalculateRebateResult();
+            rebateDataStoreMock.Setup(r => r.GetRebate(request.RebateIdentifier)).Returns(rebate);
+            productDataStoreMock.Setup(p => p.GetProduct(request.ProductIdentifier)).Returns(product);
+            var rebateService = new RebateService(rebateDataStoreMock.Object, productDataStoreMock.Object, incentiveCalculatorsMock);
+
+            // Act
+            var calculationResult = rebateService.Calculate(request);
+
+            // Assert
+            Assert.True(calculationResult.Success);
+        }
+
+        [Fact]
+        public void Calculate_FixedRateRebate_ReturnsSuccess()
+        {
+            // Arrange
+            var rebateDataStoreMock = new Mock<IRebateDataStore>();
+            var productDataStoreMock = new Mock<IProductDataStore>();
+            var incentiveCalculatorsMock = new IncentiveCalculators();
+
+            rebateDataStoreMock.Setup(r => r.GetRebate(It.IsAny<string>()))
+                .Returns((string rebateIdentifier) =>
+                {
+                    return new Rebate()
+                    {
+                        Incentive = IncentiveType.FixedRateRebate,
+                        Amount = 10m,
+                        Percentage = 0.05m // Set appropriate percentage value
+                    };
+                });
+
+            productDataStoreMock.Setup(p => p.GetProduct(It.IsAny<string>()))
+                .Returns((string productIdentifier) =>
+                {
+                    return new Product
+                    {
+                        SupportedIncentives = SupportedIncentiveType.FixedRateRebate
+                    };
+                });
+
+            var request = new CalculateRebateRequest
+            {
+                RebateIdentifier = "1",
+                ProductIdentifier = "A",
+                Volume = 500
+            };
+            var rebate = new Rebate
+            {
+                Incentive = IncentiveType.FixedRateRebate,
+                Amount = 10m,
+                Percentage = 0.05m // Set appropriate percentage value
+            };
+
+            var product = new Product
+            {
+                SupportedIncentives = SupportedIncentiveType.FixedRateRebate
+            };
+            var result = new CalculateRebateResult();
+            rebateDataStoreMock.Setup(r => r.GetRebate(request.RebateIdentifier)).Returns(rebate);
+            productDataStoreMock.Setup(p => p.GetProduct(request.ProductIdentifier)).Returns(product);
+            var rebateService = new RebateService(rebateDataStoreMock.Object, productDataStoreMock.Object, incentiveCalculatorsMock);
+
+            // Act
+            var calculationResult = rebateService.Calculate(request);
+
+            // Assert
+            Assert.True(calculationResult.Success);
+        }
+
+        [Fact]
+        public void Calculate_AmountPerUom_ReturnsSuccess()
+        {
+            // Arrange
+            var rebateDataStoreMock = new Mock<IRebateDataStore>();
+            var productDataStoreMock = new Mock<IProductDataStore>();
+            var incentiveCalculatorsMock = new IncentiveCalculators();
+
+            rebateDataStoreMock.Setup(r => r.GetRebate(It.IsAny<string>()))
+                .Returns((string rebateIdentifier) =>
+                {
+                    return new Rebate()
+                    {
+                        Incentive = IncentiveType.AmountPerUom,
+                        Amount = 10m
+                    };
+                });
+
+            productDataStoreMock.Setup(p => p.GetProduct(It.IsAny<string>()))
+                .Returns((string productIdentifier) =>
+                {
+                    return new Product
+                    {
+                        SupportedIncentives = SupportedIncentiveType.AmountPerUom
+                    };
+                });
+
+            var request = new CalculateRebateRequest
+            {
+                RebateIdentifier = "1",
+                ProductIdentifier = "A",
+                Volume = 500
+            };
+            var rebate = new Rebate
+            {
+                Incentive = IncentiveType.AmountPerUom,
+                Amount = 10m
+            };
+
+            var product = new Product
+            {
+                SupportedIncentives = SupportedIncentiveType.AmountPerUom
+            };
+            var result = new CalculateRebateResult();
+            rebateDataStoreMock.Setup(r => r.GetRebate(request.RebateIdentifier)).Returns(rebate);
+            productDataStoreMock.Setup(p => p.GetProduct(request.ProductIdentifier)).Returns(product);
+            var rebateService = new RebateService(rebateDataStoreMock.Object, productDataStoreMock.Object, incentiveCalculatorsMock);
+
+            // Act
+            var calculationResult = rebateService.Calculate(request);
+
+            // Assert
+            Assert.True(calculationResult.Success);
+        }
+
+    }
+}
